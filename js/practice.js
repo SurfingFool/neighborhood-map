ae148f7cd4c2d51a9f07cf3a8bd7b0f79db38666
Brett Ludwick <ludwickscience@gmail.com>
2016-05-17 06:45:02 -0700

    Add List Attempt

--- js/practice.js
+++ js/practice.js
@@ -1,108 +1,123 @@
-/*==== Model or Data ====*/
-
-var model =  {
-		locations: [
-				{
-					name : 'Fish House Vera Cruz',
-					lat : 33.136278,
-					lng : -117.188976,
-					info : 'Delicious fresh fish in a cozy atmosphere.'
-				},
-				{
-					name : 'Mama Kats Restaurant',
-					lat : 33.135647,
-					lng : -117.186735,
-					info : 'Homemade food at reasonable prices.'
-				},
-				{
-					name : 'Toms Burger Family Restaurant',
-					lat : 33.132742,
-					lng : -117.194078,
-					info : 'Burgers, fries, malts and shakes.'
-				},
-				{
-					name : 'Elephant Bar',
-					lat : 33.140122,
-					lng : -117.192620,
-					info : 'A variety of American, Italian, and Mexican cuisine.'
-				},
-				{
-					name : 'Nattiya Thai Restaurant',
-					lat : 33.136045,
-					lng : -117.179256,
-					info : 'Authentic Thai Food and specialty drink bar.'
-				},
-				{
-					name : 'Cocina Del Charro',
-					lat : 33.135069,
-					lng : -117.189914,
-					info : 'Specialty Mexican dishes, outdoor patio, and bar.'
-				}
-		]
-};
+/*==== Model ====*/
+var locations = [
+	{
+		name : 'Fish House Vera Cruz',
+		lat : 33.136278,
+		lng : -117.188976,
+		info : 'Delicious fresh fish in a cozy atmosphere.'
+	},
+	{
+		name : 'Mama Kats Restaurant',
+		lat : 33.135647,
+		lng : -117.186735,
+		info : 'Homemade food at reasonable prices.'
+	},
+	{
+		name : 'Toms Burger Family Restaurant',
+		lat : 33.132742,
+		lng : -117.194078,
+		info : 'Burgers, fries, malts and shakes.'
+	},
+	{
+		name : 'Elephant Bar',
+		lat : 33.140122,
+		lng : -117.192620,
+		info : 'A variety of American, Italian, and Mexican cuisine.'
+	},
+	{
+		name : 'Nattiya Thai Restaurant',
+		lat : 33.136045,
+		lng : -117.179256,
+		info : 'Authentic Thai Food and specialty drink bar.'
+	},
+	{
+		name : 'Cocina Del Charro',
+		lat : 33.135069,
+		lng : -117.189914,
+		info : 'Specialty Mexican dishes, outdoor patio, and bar.'
+	}
+]
 
 
-/*==== View Model ====*/
-
-
-
-/*==== View ====*/
-
-
-function initMap() {
-		// Object that defines map properties.
-		var sanMarcos = {lat: 33.136, lng: -117.189};
-		var mapOptions = {
-				center: sanMarcos,
-				zoom: 15,
-				mapTypeId:google.maps.MapTypeId.ROADMAP
-		};
-		// Creates map inside #mapDiv element with myOptions parameters passed to it.
-		var map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
-
-		// Creates the search box, link to UI element, and returns predicted search terms.
-		// var input = document.getElementById("search");
-		// var searchBox = new google.maps.places.SearchBox(input);
-
-		// Iterates through array of locations & places each one on the map.
-		for (i = 0; i < model.locations.length; i++) {
-				var position = new google.maps.LatLng(model.locations[i].lat, model.locations[i].lng);   
-				var marker = new google.maps.Marker({
-						position: position,
-						map: map,
-						title: model.locations[i].name,
-						clickable: true
-				});
-
-				function addInfoWindow(marker, message) {
-
-						var infoWindow = new google.maps.InfoWindow({
-								content: message
-						});
-
-						google.maps.event.addListener(marker, 'click', function () {
-								infoWindow.open(map, marker);
-						});
-				}
-				addInfoWindow(marker, model.locations[i].info);
-
-		}
+// Class constructor
+var Location = function(data) {
+	var self = this;
+	self.name = data.name;
 }
 
+/*==== View Model ====*/
+var locationViewModel = function() {	
+	var self = this;
 
+	var locationItem = locations[0];
+	self.currentLocation = ko.observable(new Location(locationItem));
 
-function listView() {
-		var locationList = model.locations[i].name;
-		// Iterate over the locations
-		for (i = 0; i < model.locations.length; i++) {
-				locationList += model.locations[i].name;
-				function init() {
- 				this.locationsList = document.getElementById('location-list')
+	console.log(self.currentLocation());
+	console.log(self.currentLocation().name);
+	
+	//for (i = 0; i < locations.length; i++);
+		//location = locations[i];
+	// var places = '<ul>';
+	// for (var index in locations) {
+	// 	places += '<li>' + locations[i].name + '</li>';
+	// }
+	// places += '</ul>'
+}
 
-		 		}
+ko.applyBindings(new locationViewModel);
+	
+
+var initMap = function() {
+	// Defines map properties.
+	var sanMarcos = {lat: 33.136, lng: -117.189};
+	var mapOptions = {
+		center: sanMarcos,
+		zoom: 15,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	
+	// Creates map inside #mapDiv element with myOptions parameters passed to it.
+	var map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
+	// Shows markers
+	
+
+	// var searchBox = new google.maps.places.SearchBox(input);
+	// ko.applyBindings(new initMap());	
+
+	// Adds markers and corresponding info windows to locations when clicked
+	var addMarkers = function() {
+		var marker;
+		// Iterates through array of locations & places each one on the map.
+		for (i = 0; i < locations.length; i++) {
+			var position = new google.maps.LatLng(locations[i].lat, locations[i].lng);   
+			marker = new google.maps.Marker({
+				position: position,
+				map: map,
+				title: locations[i].name,
+				clickable: true
+			});
+
+			function addInfoWindow(marker, message) {
+
+				var infoWindow = new google.maps.InfoWindow({
+						content: message
+				});
+
+				google.maps.event.addListener(marker, 'click', function () {
+						infoWindow.open(map, marker);
+				});
+			}
+			addInfoWindow(marker, locations[i].info);
 		}
+	}
+}
+	
 
-} 
-// Invokes initMap function on window load to render map.
+//Invokes initMap function on window load to render map.
 google.maps.event.addDomListener(window, 'load', initMap);
-listview();
+// window.onload = initMap();
+
+
+
+
+
