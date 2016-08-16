f3e32a174c7f83f4de7b211768bf0f8d71e59535
Brett Ludwick <ludwickscience@gmail.com>
2016-08-16 08:28:29 -0700

    Delete practice.js

--- js/practice.js
+++ js/practice.js
@@ -1,5 +1,5 @@
 // Class Constructor
-function LocationItem(name, lat, lng, info) {
+function LocationItem(name, location, info) {
     var self = this;
     
     self.name = name;
@@ -17,6 +17,53 @@ var locations = ko.observableArray()[
         {name:'Cocina Del Charro', location: {lat: 33.135069, lng: -117.189914}, info: 'Specialty Mexican dishes, outdoor patio, and bar.'}
 ];
 
+
+function initMap() {
+    // Object that defines map properties.
+    var mapOptions = {
+        center: {lat: 33.136, lng: -117.189},
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+} // End of initMap()    
+
+    // Creates map inside #mapDiv element with mapOptions parameters passed to it.
+    map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
+
+    // Creates the search box, link to UI element, and returns predicted search terms.
+    // var input = document.getElementById("search");
+    // var searchBox = new google.maps.places.SearchBox(input);
+    var smallInfoWindow = new google.maps.InfoWindow();
+    // Markers.  Iterates through array of locations & places each one on the map.
+    for (i = 0; i < locations.length; i++) {
+        var position = locations[i].location;
+        var title = locations[i].name;
+        marker = new google.maps.Marker({
+            // map: map,  // moved inside showListings function
+            position: position,
+            title: title,
+            clickable: true,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        markers.push(marker);
+
+        marker.addListener('click', function() {
+                populateInfoWindow(this, smallInfoWindow);
+        })
+
+function addInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.name + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        infowindow.addListener('closeclick',function(){
+            infowindow.setMarker(null);
+        });
+    }
+}
 /*==== View Model ====*/
 function viewModel() {
 
@@ -26,56 +73,8 @@ function viewModel() {
     var map;
     var marker;
     var markers = [];
-
-    function initMap() {
-        // Object that defines map properties.
-        var mapOptions = {
-            center: {lat: 33.136, lng: -117.189},
-            zoom: 15,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        
-        // Creates map inside #mapDiv element with mapOptions parameters passed to it.
-        map = new google.maps.Map(document.getElementById("mapDiv"), mapOptions);
-
-        // Creates the search box, link to UI element, and returns predicted search terms.
-        // var input = document.getElementById("search");
-        // var searchBox = new google.maps.places.SearchBox(input);
-        var smallInfoWindow = new google.maps.InfoWindow();
-        // Markers.  Iterates through array of locations & places each one on the map.
-        for (i = 0; i < locations.length; i++) {
-            var position = locations[i].location;
-            var title = locations[i].name;
-            marker = new google.maps.Marker({
-                // map: map,  // moved inside showListings function
-                position: position,
-                title: title,
-                clickable: true,
-                animation: google.maps.Animation.DROP,
-                id: i
-            });
-            markers.push(marker);
-
-            marker.addListener('click', function() {
-                    populateInfoWindow(this, smallInfoWindow);
-            })
-
-        function addInfoWindow(marker, infowindow) {
-            // Check to make sure the infowindow is not already opened on this marker.
-            if (infowindow.marker != marker) {
-                infowindow.marker = marker;
-                infowindow.setContent('<div>' + marker.name + '</div>');
-                infowindow.open(map, marker);
-                // Make sure the marker property is cleared if the infowindow is closed.
-                infowindow.addListener('closeclick',function(){
-                    infowindow.setMarker(null);
-                });
-            }
-        }
-    } // End of initMap()
-    initMap();
+    
 }
-// Invokes initMap function on window load to render map.
-// google.maps.event.addDomListener(window, 'load', initMap);    
+   
    
 ko.applyBindings(new viewModel());    
